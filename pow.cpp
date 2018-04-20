c8cfe335e7285e305400be85aa83af534437d297
sathyaruba <36071234+satthyaruba@users.noreply.github.com>
2018-04-20 15:34:37 +0530

    Create pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,18 @@
+# #include<iostream>
+using namespace std;
+int main()
+{
+    int p,p1,ret=0,n,i;
+    p=2;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        p1=n*p;
+        
+        //ret=p1+ret;
+
+    }
+    cout<<p1;
+return 0;    
+    
+}
